d9727e891ad91cf2338274daa40389b2e89d5ee8
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2021-01-27 02:04:30 +0100

    nothing

--- server/server.js
+++ server/server.js
@@ -40,6 +40,5 @@ app.use('/api/auth',require('../routes/api/auth'))
 app.use('/api/posts',require('../routes/api/posts'))
 app.use('/api/profile',require('../routes/api/profile'))
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server running on PORT:',PORT));
